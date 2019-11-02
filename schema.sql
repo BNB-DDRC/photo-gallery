5240ff2800e1ca0ff24c8ef33447258756df6f39
Candy Wang <candywang013@gmail.com>
2019-11-01 19:55:08 -0700

    Implement schema for postgreSQL

---
+++ schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS photo_gallery;
+CREATE DATABASE photo_gallery;
+
+CREATE TABLE listings (
+    listing_id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE photos (
+    listing_id INTEGER NOT NULL,
+    photo_id SERIAL NOT NULL PRIMARY KEY, // BIG SERIAL?
+    photo_url TEXT NOT NULL,
+    caption VARCHAR(250) NOT NULL,
+    FOREIGN KEY (listing_id) REFERENCES listings (listing_id) ON DELETE CASCADE
+);
